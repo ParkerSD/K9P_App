ca384d987ca4b8d5ae347136578d1ce0d3691f30
Parker Davis <parkersamueldavis@gmail.com>
2021-06-23 21:38:21 -0700

    Updates

---
+++ precache-manifest.6d13e34186825fbee9a563a7e3ec2a13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07af18d4f88c5611ccf2acb762a25192",
+    "url": "/k9p_app/src/App.js/index.html"
+  },
+  {
+    "revision": "9e443afa08a0ff546cb5",
+    "url": "/k9p_app/src/App.js/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "77c566f1c0ddc8009997",
+    "url": "/k9p_app/src/App.js/static/js/2.3efd4cba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/k9p_app/src/App.js/static/js/2.3efd4cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e443afa08a0ff546cb5",
+    "url": "/k9p_app/src/App.js/static/js/main.f6d90ec1.chunk.js"
+  },
+  {
+    "revision": "d18f26e203f2c1f9a48a",
+    "url": "/k9p_app/src/App.js/static/js/runtime-main.9dfb61e5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/k9p_app/src/App.js/static/media/logo.ee7cd8ed.svg"
+  }
+]);
