7083dd6750b082b990607d255c13065dc1667800
Parker Davis <parkersamueldavis@gmail.com>
2021-06-23 21:44:39 -0700

    Updates

---
+++ precache-manifest.f6f3a09925a1c76c7414f25fcbafbaa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d31fed8ba486f9bb8d882380ba49df1",
+    "url": "/k9p_app/index.html"
+  },
+  {
+    "revision": "01b128700f3117cad728",
+    "url": "/k9p_app/static/css/main.643b110a.chunk.css"
+  },
+  {
+    "revision": "77c566f1c0ddc8009997",
+    "url": "/k9p_app/static/js/2.3efd4cba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/k9p_app/static/js/2.3efd4cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01b128700f3117cad728",
+    "url": "/k9p_app/static/js/main.0599d734.chunk.js"
+  },
+  {
+    "revision": "7ee50cd4528a97816111",
+    "url": "/k9p_app/static/js/runtime-main.87a45af0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/k9p_app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
