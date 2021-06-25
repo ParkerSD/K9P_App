5e00ae2c3ee187b7f05914c81d600af46ef0bdef
Parker Davis <parkersamueldavis@gmail.com>
2021-06-25 01:08:49 -0700

    v3 build

---
+++ build/precache-manifest.2ebcf897e8417cb82615cd07ab0fc26f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f58ce986a6d1b2c61c8715b286c3046",
+    "url": "/k9p_app/index.html"
+  },
+  {
+    "revision": "913e610b9c3fdbb6e0c8",
+    "url": "/k9p_app/static/css/main.643b110a.chunk.css"
+  },
+  {
+    "revision": "34e97de98b1b9a0be7bf",
+    "url": "/k9p_app/static/js/2.577e02ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/k9p_app/static/js/2.577e02ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913e610b9c3fdbb6e0c8",
+    "url": "/k9p_app/static/js/main.1fe1b55b.chunk.js"
+  },
+  {
+    "revision": "7ee50cd4528a97816111",
+    "url": "/k9p_app/static/js/runtime-main.87a45af0.js"
+  }
+]);
